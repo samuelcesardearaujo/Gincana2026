25086519cb29da1a23f3a40ea6cbd5e5a3b213ec
Samuel Cesar <80364653+samuelcesardearaujo@users.noreply.github.com>
2026-09-23 10:53:11 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://SUO_PROJETO.supabase.co';
-const supabaseAnonKey = 'SUA_CHAVE_ANON_PUBLIC';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
